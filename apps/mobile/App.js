419c47585f49a701957bb40aff000eb4548e0d70
Yuefii <muffidahmadiawan31@gmail.com>
2023-11-04 21:29:09 +0700

    refactor: fix data json

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -6,10 +6,12 @@ export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="auto" backgroundColor="#2F80ED" />
+      <View style={{ flex: 1 }} backgroundColor="#2F80ED">
       <WebView
         style={{ marginTop: 32 }}
         source={{ uri: "https://sample-jadkul.vercel.app/" }}
-      />
+        />
+      </View>
     </View>
   );
 }
