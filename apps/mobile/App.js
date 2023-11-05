dc422f63c40612412ab5af5094d0994cee374123
Yuefii <muffidahmadiawan31@gmail.com>
2023-11-05 13:14:17 +0700

    remove notification cuz lots a bug

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,35 +1,8 @@
-import { View, Button } from "react-native";
+import { View } from "react-native";
 import { WebView } from "react-native-webview";
 import { StatusBar } from "expo-status-bar";
-import * as Notifications from "expo-notifications";
-import React, { useEffect } from "react";
 
 export default function App() {
-  const notificationContent = {
-    title: 'Cek Kegiatan anda hari',
-    body: 'Siapa tau ada jadwal kuliah',
-    sound: 'default',
-  };
-
-  const sendNotification = async () => {
-    await Notifications.scheduleNotificationAsync({
-      content: notificationContent,
-      trigger: null,
-    });
-  };
-
-  const getNotificationPermission = async () => {
-    const { granted } = await Notifications.requestPermissionsAsync();
-    if (!granted) {
-      console.log('Izin notifikasi ditolak!');
-    }
-  };
-
-  useEffect(() => {
-    getNotificationPermission();
-  }, []);
-
-
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="auto" backgroundColor="#2F80ED" />
@@ -39,7 +12,6 @@ export default function App() {
           source={{ uri: "https://sample-jadkul.vercel.app/" }}
         />
       </View>
-      <Button title="Kirim Notifikasi" onPress={sendNotification} />
     </View>
   );
 }
